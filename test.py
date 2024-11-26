ff69a08096e5813af040201b20b36857dc01afb1
nadine.loepfe <nadine.loepfe@swirldslabs.com>
2024-11-26 13:40:25 +0000

    hardcoding to test this

--- test.py
+++ test.py
@@ -127,7 +127,7 @@ def transfer_token(client, recipient_id, token_id):
 def main():
     operator_id, operator_key = load_operator_credentials()
 
-    network = Network(os.getenv('NETWORK', 'solo'))
+    network = Network(node_address='localhost:50211', node_account_id=AccountId(0, 0, 3))
     client = Client(network)
     client.set_operator(operator_id, operator_key)
 
